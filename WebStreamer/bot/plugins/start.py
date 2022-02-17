669cce2c80188dda508ee0fad5b5cd1fc60b7137
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-17 12:51:30 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,8 +6,16 @@ from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
 
 
-@StreamBot.on_message(filters.command(["start", "help"]))
+@StreamBot.on_message(filters.command(["start"]))
 async def start(_, m: Message):
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+        f"""Hᴇʟʟᴏ 👋 {m.from_user.mention(style="md")}, 
+
+Tʜɪs Is A Fɪʟᴇ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Bᴏᴛ
+
+Yᴏᴜ Cᴀɴ Usᴇ Mᴇ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Fɪʟᴇs Fʀᴏᴍ Yᴏᴜ'ʀᴇ Bʀᴏᴡsᴇʀ
+
+Jᴜsᴛ Sᴇɴᴅ Mᴇ A Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ Aɴᴅ Sᴇᴇ Mʏ Pᴏᴡᴇʀ
+
+Pᴏᴡᴇʀᴇᴅ ʙʏ : @AIOM_BOTS"""
     )
