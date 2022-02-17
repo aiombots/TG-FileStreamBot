eb759e3e20f25bde16e5ba373a53916814f4e928
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-02-17 12:59:54 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -2,14 +2,20 @@
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
 from WebStreamer.bot import StreamBot
 
 
 @StreamBot.on_message(filters.command(["start"]))
 async def start(_, m: Message):
-    await m.reply(
-        f"""Hᴇʟʟᴏ 👋 {m.from_user.mention(style="md")}, 
+    button = [[
+      InlineKeyboardButton("Gʀᴏᴜᴘ", url="https://t.me/AIOM_BOTS_GROUP"),
+      InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/AIOM_BOTS")
+      ],[
+      InlineKeyboardButton("Cʟᴏsᴇ", url="https://t.me/AIOM_BOTS")
+      ]]
+    await m.reply_text(
+        text=f"""Hᴇʟʟᴏ 👋 {m.from_user.mention(style="md")}, 
 
 Tʜɪs Is A Fɪʟᴇ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Bᴏᴛ
 
@@ -18,4 +24,5 @@ Yᴏᴜ Cᴀɴ Usᴇ Mᴇ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Fɪʟᴇs Fʀᴏᴍ Yᴏᴜ'
 Jᴜsᴛ Sᴇɴᴅ Mᴇ A Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ Aɴᴅ Sᴇᴇ Mʏ Pᴏᴡᴇʀ
 
 Pᴏᴡᴇʀᴇᴅ ʙʏ : @AIOM_BOTS"""
+        reply_markup=InlineKeyboardMarkup(button)
     )
